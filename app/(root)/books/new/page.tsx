ec53bc939bf78ae246906aba94b7dc2e6ac455af
a-wahab-jafar <a.wahab.jafar1@gmail.com>
2026-02-28 21:43:47 +0300

    set up the app and implement homepage and new book ui

---
+++ app/(root)/books/new/page.tsx
@@ -0,0 +1,21 @@
+import UploadForm from '@/components/UploadForm'
+
+
+const Page = () => {
+  return (
+    <main className='wrapper container'>
+        <div className='mx-auto max-w-180 space-y-10'>
+            <section className=' flex flex-col gap-5'>
+                <h1 className='page-title-xl'>
+                    Add a New Book
+                </h1>
+                <p>Upload a PDF to Generate your interactive interview</p>
+            </section>
+            
+            <UploadForm />
+        </div>
+    </main>
+  )
+}
+
+export default Page
